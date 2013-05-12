7039241259493de09f4053a3a80c04100a342d79
Mike Henke <henke.mike@gmail.com>
2013-05-11 19:29:48 -0500

    changes

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,6 +1,6 @@
-# cloudy Attributes
-default['cloudy']['install_path'] = "/vagrant/wwwroot"
-default['cloudy']['owner'] = nil
-default['cloudy']['group'] = "bin"
-default['cloudy']['download']['url'] = "http://varscoper.riaforge.org/index.cfm?event=action.download"
-default['cloudy']['create_apache_alias'] = false
+# varscoper Attributes
+default['varscoper']['install_path'] = "/vagrant/wwwroot"
+default['varscoper']['owner'] = nil
+default['varscoper']['group'] = "bin"
+default['varscoper']['download']['url'] = "http://varscoper.riaforge.org/index.cfm?event=action.download"
+default['varscoper']['create_apache_alias'] = false
