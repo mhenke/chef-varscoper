f24afd93d769443a9bd665e4a901d45cb816f194
Mike Henke <henke.mike@gmail.com>
2013-05-11 23:48:17 -0500

    changes

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email "henke.mike@gmail.com"
 license          "Apache 2.0"
 description      "Installs/Configures varscoper"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "0.0.613"
+version          "0.0.614"
 
 %w{ centos redhat ubuntu }.each do |os|
   supports os
